b1b1e924265a61f7c7768ab146ed51a01b48f5fe
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-07-04 10:50:57 +0100

    Updated installer to use the README file as the description.

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
+import os
+
 from setuptools import setup, find_packages
 
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
 setup(name='django-startproject-plus',
-      version='0.1a',
+      version='0.1b.1',
       description='Django superset startproject command for creating '
       'new projects.',
-      long_description='Drop-in replacement for django-admin.py supersets '
-      'the command by adding extra_context support.',
+      long_description=README,
       author='Alfredo Aguirre',
       author_email='hello@madewithbyt.es',
       license='BSD License',
       url='https://github.com/alfredo/django-startproject-plus',
       include_package_data=True,
+      package_data={
+          'django-startproject_plus': [
+              'django-startproject.py',
+          ]
+      },
+      zip_safe=False,
       scripts=['django-startproject.py'],
       classifiers=[
           'Development Status :: 3 - Alpha',
@@ -22,9 +31,6 @@ setup(name='django-startproject-plus',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
           'Topic :: Internet :: WWW/HTTP',
-          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-          'Topic :: Internet :: WWW/HTTP :: WSGI',
-          'Topic :: Software Development :: Libraries :: Application Frameworks',
           'Topic :: Software Development :: Libraries :: Python Modules',
       ],
       packages=find_packages(exclude=['tests']),
